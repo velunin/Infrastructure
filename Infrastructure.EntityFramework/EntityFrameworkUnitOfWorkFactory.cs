2be394962912d823f184203f3b964884bae7bd79
sevaelunin <seva.elunin@gmail.com>
2016-03-25 17:56:20 +0300

    add uow factory

---
+++ Infrastructure.EntityFramework/EntityFrameworkUnitOfWorkFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using Infrastructure.Domain;
+
+namespace Infrastructure.EntityFramework
+{
+    public class EntityFrameworkUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private readonly IDbContextFactory _contextFactory;
+
+        public EntityFrameworkUnitOfWorkFactory(IDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public IUnitOfWork Create(IsolationLevel isolation)
+        {
+            return new EntityFrameworkUnitOfWork(_contextFactory) { IsolationLevel = isolation };
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new EntityFrameworkUnitOfWork(_contextFactory);
+        }
+    }
+}
