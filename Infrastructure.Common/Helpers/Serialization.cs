fe0140729ec46120ffe73bae0a0dcfd3a96d4964
sevaelunin <seva.elunin@gmail.com>
2016-04-13 09:43:46 +0300

    add serialization helpers

---
+++ Infrastructure.Common/Helpers/Serialization.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Infrastructure.Common.Helpers
+{
+    public class Serialization
+    {
+        public static byte[] Serialize<T>(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Flush();
+                stream.Position = 0;
+                return stream.ToArray();
+            }
+        }
+
+        public static T Deserialize<T>(byte[] array)
+        {
+            using (var stream = new MemoryStream(array))
+            {
+                var formatter = new BinaryFormatter();
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
