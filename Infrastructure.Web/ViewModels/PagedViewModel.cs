e288aaf6e6cacbb868d567a2e94070e6ce8785d3
vsel <vsel@manzanagroup.ru>
2016-04-21 16:22:12 +0300

    new project for common web classes

---
+++ Infrastructure.Web/ViewModels/PagedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure.Web.ViewModels
+{
+    public class PagedViewModel<TModel>
+    {
+        public IEnumerable<TModel> Items { get; set; }
+        public int TotalRows { get; set; }
+        public int TotalPages { get; set; }
+        public int PageIndex { get; set; }
+        public int PageNum { get; set; }
+        public int PageSize { get; set; }
+    }
+}
